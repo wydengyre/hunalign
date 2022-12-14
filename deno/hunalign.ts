1a18d37484acb60a6bb2935736724332eca089ae
wydengyre <108271225+wydengyre@users.noreply.github.com>
2022-12-14 14:18:31 +0000

    DenoHunalign class

--- deno/hunalign.ts
+++ deno/hunalign.ts
@@ -1,15 +1,44 @@
-import { Hunalign } from "../ts/hunalign.ts";
-import { fromFileUrl } from "https://deno.land/std@0.167.0/path/mod.ts";
+import {Hunalign, Ladder} from "../ts/hunalign.ts";
+
+export class DenoHunalign {
+  #hunalign: Hunalign;
+
+  private constructor(hunalign: Hunalign) {
+    this.#hunalign = hunalign;
+  }
+
+  run(
+    dictionary: Uint8Array,
+    source: Uint8Array,
+    target: Uint8Array,
+  ): Ladder {
+    return this.#hunalign.run(dictionary, source, target);
+  }
+
+  async runWithPaths(dictPath: string, sourcePath: string, targetPath: string): Promise<Ladder> {
+    const [dict, source, target] = await Promise.all([
+      Deno.readFile(dictPath),
+      Deno.readFile(sourcePath),
+      Deno.readFile(targetPath)
+    ]);
+    return this.run(dict, source, target);
+  }
+
+  static async createWithWasmBinary(wasmBinary: Uint8Array): Promise<DenoHunalign> {
+    const hunalign = await Hunalign.create(wasmBinary);
+    return new DenoHunalign(hunalign);
+  }
+
+  static async createWithWasmPath(wasmPath: string): Promise<DenoHunalign> {
+    const wasmBinary = await Deno.readFile(wasmPath);
+    return this.createWithWasmBinary(wasmBinary);
+  }
+}
 
 const wasmPath = "../web/gen/hunalign.wasm";
 const dictPath = "../test/hunapertium-eng-fra.dic";
 const frenchPath = "../test/chapitre.sentences.txt";
 const englishPath = "../test/chapter.sentences.txt";
 
-const [wasmBinary, dict, french, english] = await Promise.all(
-  [wasmPath, dictPath, frenchPath, englishPath]
-    .map((path) => Deno.readFile(fromFileUrl(import.meta.resolve(path)))),
-);
-
-const hunalign = await Hunalign.create(wasmBinary);
-console.log(hunalign.run(dict, french, english));
+const hunalign = await DenoHunalign.createWithWasmPath(wasmPath);
+console.log(await hunalign.runWithPaths(dictPath, frenchPath, englishPath));
--- ts/hunalign.ts
+++ ts/hunalign.ts
@@ -11,6 +11,9 @@ type Module = {
   };
 };
 
+export type Rung = [number, number, number];
+export type Ladder = Rung[];
+
 export class Hunalign {
   #module: Module;
   #printLog: string[];
@@ -24,23 +27,34 @@ export class Hunalign {
     dictionary: Uint8Array,
     source: Uint8Array,
     target: Uint8Array,
-  ): string[] {
+  ): Ladder {
     this.#module.FS.writeFile("/dictionary", dictionary);
     this.#module.FS.writeFile("/source", source);
     this.#module.FS.writeFile("/target", target);
     this.#module.callMain(["dictionary", "source", "target"]);
-    return this.#printLog;
+
+    return toLadder(this.#printLog);
   }
 
-  static async create(wasmBinary: Uint8Array): Promise<Hunalign> {
+  static async create(wasmBinary?: Uint8Array): Promise<Hunalign> {
     const printLog: string[] = [];
     function print(text: string): void {
       printLog.push(text);
     }
 
-    // TODO: can this be made sync? Docs mention a trick.
-    const module = await mkHunalign({ print, wasmBinary });
+    const module = await mkHunalign({print, wasmBinary});
     return new Hunalign(module, printLog);
   }
 }
 
+function toLadder(prints: string[]): Ladder {
+  return prints.map((s) => toRung(s));
+}
+
+function toRung(s: string): Rung {
+  const nums = s.split("\t");
+  if (nums.length !== 3) {
+    throw `could not convert ${nums} to rung`;
+  }
+  return [parseInt(nums[0]), parseInt(nums[1]), Number.parseFloat(nums[2])];
+}
