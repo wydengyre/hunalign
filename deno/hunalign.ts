276eb9b75a4f021421a006be1642d5557041fe31
wydengyre <108271225+wydengyre@users.noreply.github.com>
2022-12-14 12:08:34 +0000

    move test data

--- deno/hunalign.ts
+++ deno/hunalign.ts
@@ -2,9 +2,9 @@ import { Hunalign } from "../ts/hunalign.ts";
 import { fromFileUrl } from "https://deno.land/std@0.167.0/path/mod.ts";
 
 const wasmPath = "../web/gen/hunalign.wasm";
-const dictPath = "../web/test/hunapertium-eng-fra.dic";
-const frenchPath = "../web/test/chapitre.sentences.txt";
-const englishPath = "../web/test/chapter.sentences.txt";
+const dictPath = "../test/hunapertium-eng-fra.dic";
+const frenchPath = "../test/chapitre.sentences.txt";
+const englishPath = "../test/chapter.sentences.txt";
 
 const [wasmBinary, dict, french, english] = await Promise.all(
   [wasmPath, dictPath, frenchPath, englishPath]
